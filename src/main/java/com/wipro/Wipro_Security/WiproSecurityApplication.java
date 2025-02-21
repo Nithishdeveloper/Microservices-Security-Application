ecd20f71170311c53d7c9b4e84ecbdb720749d94
Nithishdeveloper <nithishchml123@gmail.com>
2025-02-21 17:56:12 +0530

    git commit

--- src/main/java/com/wipro/Wipro_Security/WiproSecurityApplication.java
+++ src/main/java/com/wipro/Wipro_Security/WiproSecurityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WiproSecurityApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WiproSecurityApplication.class, args);
 	}
-
 }
